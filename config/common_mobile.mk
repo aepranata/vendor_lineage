634aef55d14974f48b4a8bb9ee2394cda2eaf0f6
Michael Bestas <mkbestas@lineageos.org>
2024-12-11 07:34:05 +0200

    Revert^3 "config: Build NavigationBarMode2ButtonOverlay"
    This reverts commit da5e531ac35a199b4564f715e50f99f4a4360d65.
    
    Change-Id: Iae1c28582c2130a683be314810ee9201c53cccf5
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -82,8 +82,7 @@ PRODUCT_PACKAGES += \
     IconShapeTaperedRectOverlay \
     IconShapeTeardropOverlay \
     IconShapeVesselOverlay \
-    LineageNavigationBarNoHint \
-    NavigationBarMode2ButtonOverlay
+    LineageNavigationBarNoHint
 
 # Legal
 PRODUCT_SYSTEM_PROPERTIES += \
